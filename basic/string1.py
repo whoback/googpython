ba4d16290778d012d681e502e4aa5e39eba177ce
Will Hoback <will.hoback@gmail.com>
2014-12-31 20:39:09 -0600

    fix_start problem

--- basic/string1.py
+++ basic/string1.py
@@ -52,8 +52,13 @@ def both_ends(s):
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
-  # +++your code here+++
-  return
+  stra = s[0]
+  strb = "*"
+  for letter in s:
+    s.find(stra)
+    s.replace(stra, strb)
+
+  return s
 
 
 # D. MixUp
